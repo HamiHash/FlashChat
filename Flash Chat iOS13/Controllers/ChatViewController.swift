be72974eaa58395bb733ac84be21e41afa1df231
hamihash <hami.hash@yahoo.com>
2023-07-02 22:09:45 +0330

    chat bubbles adjusted, conected to firebase

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "Hami", body: "HI"),
+        Message(sender: "Tania", body: "get back home you idiot youa are xomning back home right nowwwwwwwwwww!!!!!!!!!!!!1"),
+        Message(sender: "Alex", body: "see you there")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        let titel = K.appName
+        navigationItem.hidesBackButton = true
+        navigationItem.title = titel
 
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+    
     }
     
+    @IBAction func LogoutButton(_ sender: UIBarButtonItem) {
+        do {
+          try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
+}
 
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = ""
+        let title = K.appName
+        var delayIndex = 0.0
+        for letter in title {
+            Timer.scheduledTimer(withTimeInterval: 0.2 * delayIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            delayIndex += 1
+        }
+        
+        
     }
     
-
 }
